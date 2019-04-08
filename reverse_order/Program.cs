3f4bfc7e584391a65179cf72b2f46b2d80ccd466
milomacphail <milomacphail@gmail.com>
2019-04-07 23:10:57 -0400

    Integer order successfully reversed

---
+++ reverse_order/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace reverse_order
+{
+        public static class mainClass
+        {
+            private static int Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to reverse: ");
+                Number = int.Parse(Console.ReadLine());
+                int reverseNumber = 0;
+                while (Number > 0)
+                {
+                    int remainder = Number % 10;
+                    reverseNumber = (reverseNumber * 10) + remainder;
+                    Number = Number / 10;
+                }
+            
+            Console.WriteLine(reverseNumber);
+
+        }
+    }
+}
